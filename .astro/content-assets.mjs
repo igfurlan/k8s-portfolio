009388207da0a25ed0b794c7b7cbf93a16a9e46e
Igor Furlan <igfurlan@live.com>
2026-02-24 16:41:24 +0100

    fix: Fixed page layout and some information

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_Z2gGaEW from "../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2gGaEW]]);
+import __ASTRO_IMAGE_IMPORT_Z1TMPA9 from "../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1TMPA9]]);
 		
